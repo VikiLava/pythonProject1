4eee637d132ef97bebff636a4cc28222783700f3
VikiLava <112927540+VikiLava@users.noreply.github.com>
2024-08-19 22:43:58 +0530

    Update Lab0002.py

--- src/Ex_13 08 2024_Data types and Strings/Lab0002.py
+++ src/Ex_13 08 2024_Data types and Strings/Lab0002.py
@@ -1,1 +1,24 @@
-Home work of 17 Aug
+#Home work of 17 Aug
+
+import math
+# Task 4
+# Logic Build forula
+"""Step 1 :
+input --> r Data type--> Float , radius will be entered by the user
+Py --> 3.14 or power function
+Power function or **
+output -- > Area of the circle with decimal numbers
+
+Step 2 -->
+Rough Logic
+Area = 3.14* pow (r,2)
+
+Step 3 Wirte the logic
+"""
+
+radius = float(input("Enter the Radius\n"))
+print(radius)
+print(math.pi)
+Area = 3.14 * (radius**2)
+print("The Area of the Circle is", Area)
+print(f"The Area of Circle \n {Area:.1f}")
