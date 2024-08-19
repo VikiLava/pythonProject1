50e9cb0b9bc0f1623bf24f9ee88046ccfecb385a
VikiLava <112927540+VikiLava@users.noreply.github.com>
2024-08-19 22:44:57 +0530

    Update Lab0002.py

--- src/Ex_13 08 2024_Data types and Strings/Lab0002.py
+++ src/Ex_13 08 2024_Data types and Strings/Lab0002.py
@@ -1,5 +1,7 @@
 #Home work of 17 Aug
 
+Write a Python program to calculate the area of a circle given its radius using the formula ``` area=π×r^2``` ( Take pie as 3.14)
+
 import math
 # Task 4
 # Logic Build forula
