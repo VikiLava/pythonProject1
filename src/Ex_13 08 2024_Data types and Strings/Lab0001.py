ca91311cb449ac7a662d2e6dc56d2a502f18cce9
VikiLava <112927540+VikiLava@users.noreply.github.com>
2024-08-19 21:44:04 +0530

    Update Lab0001.py

--- src/Ex_13 08 2024_Data types and Strings/Lab0001.py
+++ src/Ex_13 08 2024_Data types and Strings/Lab0001.py
@@ -15,3 +15,27 @@ print(f"{Tables}*7={Tables * 7}")
 print(f"{Tables}*8={Tables * 8}")
 print(f"{Tables}*9={Tables * 9}")
 print(f"{Tables}*10={Tables * 10}")
+
+
+#Task 2
+
+num1 = int(input("Enter the num1 "))
+num2 = int(input("Enter the num2 "))
+
+print(type(num1))
+print(type(num2))
+
+"""print("Sum is", num1 + num2)
+print("Maximum is", max(num1, num2))
+print("Sub", num1 - num2)
+print("Multi", num1 * num2)
+print("Div is", num1 / num2)
+print("pow", pow(num1, num2))"""
+
+print("Sum of "f"{num1} and "f"{num2} is", num1 + num2)
+print("Sub of "f"{num1} and "f"{num2} is", num1 - num2)
+print("Mul of "f"{num1} and "f"{num2} is", num1 * num2)
+print("div of "f"{num1} and "f"{num2} is", num1 / num2)
+#doubt how to add decimal formating
+print("power of "f"{num1} and "f"{num2} is", pow(num1, num2))
+print("Max of "f"{num1} and "f"{num2} is", max(num1, num2))
