97d37486c80ad8ceeaa65f6a9d978c44595e29c0
VikiLava <112927540+VikiLava@users.noreply.github.com>
2024-08-19 20:27:46 +0530

    Update Lab0001.py

--- src/Ex_13 08 2024_Data types and Strings/Lab0001.py
+++ src/Ex_13 08 2024_Data types and Strings/Lab0001.py
@@ -1,1 +1,17 @@
 Home work of 13 Aug
+
+Tables = int(input('Enter the User Input --'))
+# Tables = float(input('Enter the User Input --'))
+print(Tables)
+print(type(Tables))
+
+print(f"{Tables}*1={Tables}")
+print(f"{Tables}*2={Tables * 2}")
+print(f"{Tables}*3={Tables * 3}")
+print(f"{Tables}*4={Tables * 4}")
+print(f"{Tables}*5={Tables * 5}")
+print(f"{Tables}*6={Tables * 6}")
+print(f"{Tables}*7={Tables * 7}")
+print(f"{Tables}*8={Tables * 8}")
+print(f"{Tables}*9={Tables * 9}")
+print(f"{Tables}*10={Tables * 10}")
